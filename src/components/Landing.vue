d4ed4a37ea5aabe93be457e01e22f376e7f10112
Will Woodward <will@woodwardweb.com>
2023-06-29 19:08:37 +0100

    Changed profile pic and added new test chatbot

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="bg-gradient-to-tr from-zinc-800 to-zinc-900">
         <div class="w-96 mx-auto pt-64 pb-96 space-y-6">
-            <img class="rounded-full ring-2 ring-green-500 w-32" src="../assets/vue.svg">
+            <img class="rounded-full ring-2 ring-zinc-100 w-32" src="../assets/ProfilePic.jpg">
             <h1 class="uppercase font-semibold text-zinc-100 text-2xl">Will Woodward</h1>
         </div>
     </div>
