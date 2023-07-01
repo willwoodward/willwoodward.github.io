00913e2dc4a55967127ad3e64230ca03d8962a12
Will Woodward <will@woodwardweb.com>
2023-07-01 23:17:44 +0100

    shape demo

--- src/views/MainPageView.vue
+++ src/views/MainPageView.vue
@@ -1,9 +1,10 @@
 <template>
-    <Landing />
+    <iframe id="if" src="https://my.spline.design/dodecahedroncopy-f6c664c96856f2c6d7f014c637ca7417/" frameborder="0" width="100%" height="1200px"></iframe>
+    <!-- <Landing />
     <iframe id="if" src="https://my.spline.design/dodecahedron-87904bfc4121e2327f5353967829383a/" frameborder="0" width="100%" height="700px"></iframe>
     <Image3D />
     <Services />
-    <EmptySpace />
+    <EmptySpace /> -->
 </template>
 
 <style>
